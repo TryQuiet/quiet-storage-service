87a1e35149db2562968c45858258639fb951e6d7
siepra <wiktor.siepra@gmail.com>
2024-03-21 16:50:53 +0100

    fix: install pm2

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,5 +3,7 @@ curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt install nodejs -y
 
+npm install pm2@latest -g
+
 cd /home/ubuntu/qss
 npm i
