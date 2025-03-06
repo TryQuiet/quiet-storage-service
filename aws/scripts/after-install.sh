0cc11a409a479cbcf6540a6244d05d3dabed0021
Isla Koenigsknecht <ikoenigbu@gmail.com>
2025-03-06 17:38:21 -0500

    Bootstrap not build

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -2,7 +2,7 @@ cd /home/ubuntu/qss
 
 source /home/ubuntu/.bashrc
 pnpm i
-pnpm run build
+pnpm run bootstrap
 
 ENVIRONMENT=$(TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` && curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/tags/instance/Environment)
 if [ $ENVIRONMENT == "Production" ]
