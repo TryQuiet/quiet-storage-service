64f40f2a8301dc7e81fb47c9fd4451936832f804
Isla <5048549+islathehut@users.noreply.github.com>
2026-06-18 11:26:52 -0400

    Skip using aws cli for environment name

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -9,12 +9,13 @@ rm -rf node_modules/
 
 pnpm run bootstrap:deployed
 
-ENVIRONMENT=$(TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` && curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/tags/instance/Environment)
-if [ $ENVIRONMENT == "Production" ]
+# ENVIRONMENT=$(TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` && curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/tags/instance/Environment)
+ENVIRONMENT=$(cat aws-environment.txt)
+if [ $ENVIRONMENT == "production" ]
 then
   echo "Running production migrations"
   pnpm run --filter app migrate:up:prod
-elif [ $ENVIRONMENT == "Development" ]
+elif [ $ENVIRONMENT == "development" ]
 then
   echo "Running development migrations"
   pnpm run --filter app migrate:up:dev
