7c9c8379b1b3beb3ab8e62a657a788dd3ad73345
Isla <5048549+islathehut@users.noreply.github.com>
2026-06-22 11:41:33 -0400

    Move bootstrap and migration logic to app start script

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -1,31 +1,5 @@
 cd /home/qss-user
 
-echo "Bootstrapping QSS"
+echo "Changing QSS directory ownership to qss-user"
 
-chown -R qss-user .
-
-sudo su - qss-user
-
-cd /home/qss-user/qss
-
-source ~/.bashrc
-
-rm -rf dist/
-rm -rf node_modules/
-
-pnpm run bootstrap:deployed
-
-# ENVIRONMENT=$(TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` && curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/tags/instance/Environment)
-ENVIRONMENT=$(cat aws-environment.txt)
-if [ $ENVIRONMENT == "production" ]
-then
-  echo "Running production migrations"
-  pnpm run --filter app migrate:up:prod
-elif [ $ENVIRONMENT == "development" ]
-then
-  echo "Running development migrations"
-  pnpm run --filter app migrate:up:dev
-else
-  echo "Unknown environment: $ENVIRONMENT"
-  exit 1
-fi
+chown -R qss-user qss/
--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -1,6 +1,33 @@
 cd /home/qss-user/qss
 
 source ~/.bashrc
+
+echo "Clearing generated files"
+
+rm -rf dist/
+rm -rf node_modules/
+
+echo "Bootstrapping QSS"
+
+pnpm run bootstrap:deployed
+
+echo "Running database migrations"
+
+# ENVIRONMENT=$(TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` && curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/tags/instance/Environment)
+ENVIRONMENT=$(cat aws-environment.txt)
+if [ $ENVIRONMENT == "production" ]
+then
+  echo "Running production migrations"
+  pnpm run --filter app migrate:up:prod
+elif [ $ENVIRONMENT == "development" ]
+then
+  echo "Running development migrations"
+  pnpm run --filter app migrate:up:dev
+else
+  echo "Unknown environment: $ENVIRONMENT"
+  exit 1
+fi
+
 # ENVIRONMENT=$(TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` && curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/tags/instance/Environment)
 ENVIRONMENT=$(cat aws-environment.txt)
 echo "Starting $ENVIRONMENT QSS"
