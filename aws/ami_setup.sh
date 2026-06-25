a00e819aef6b78c71909226cdef1b3c47fae1434
Isla <5048549+islathehut@users.noreply.github.com>
2026-06-25 12:12:35 -0400

    Cleanup

--- aws/ami_setup.sh
+++ aws/ami_setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+###### This script is run as part of the AMI build process
+
 echo -e "Starting server init\n"
 
 ### Root Setup
--- aws/scripts/after-install.sh
+++
@@ -1,5 +0,0 @@
-cd /home/qss-user
-
-echo "Changing QSS directory ownership to qss-user"
-
-chown -R qss-user qss/
