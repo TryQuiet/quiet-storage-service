1276645ed78acd73ba543c568466899c9f48f53a
taea <taelxvie@gmail.com>
2026-08-03 15:06:20 -0400

    migrate code changes to minimal change branch api

--- app/test/utils/team.utils.ts
+++ app/test/utils/team.utils.ts
@@ -11,9 +11,6 @@ import {
   LocalUserContext,
   KeysetWithSecrets,
   generateProof,
-  MemberInvitationClaim,
-  redactDevice,
-  Base58,
 } from '@localfirst/auth'
 import { createLogger } from '../../src/nest/app/logger/logger.js'
 import { ServerKeyManagerService } from '../../src/nest/encryption/server-key-manager.service.js'
@@ -95,24 +92,10 @@ export class TeamTestUtils {
     })
     const testUserContext: LocalUserContext = { user, device }
     const invitation = testTeam.team.inviteMember()
-    const claim: MemberInvitationClaim = {
-      invitationKind: 'member',
-      userName,
-      userKeys: redactKeys(user.keys),
-      device: redactDevice(device),
-    }
-    const acceptorNonce = randomUUID().replaceAll('-', '') as Base58
     testTeam.team.admitMember(
-      generateProof({
-        seed: invitation.seed,
-        claim,
-        acceptorNonce,
-        inviteeNonce: randomUUID().replaceAll('-', '') as Base58,
-      }),
-      claim.userKeys,
-      claim.userName,
-      claim.device,
-      acceptorNonce,
+      generateProof(invitation.seed),
+      user.keys,
+      userName,
     )
     testTeam.otherUsers.push(testUserContext)
     return testTeam
